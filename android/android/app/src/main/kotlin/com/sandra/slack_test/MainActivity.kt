b76ee8ea334dec1d1eff74f4dd02cf102edbadac
Sandra-p-Thomas <118959489+Sandra-p-Thomas@users.noreply.github.com>
2023-01-19 12:37:27 +0530

    Add files via upload

---
+++ android/android/app/src/main/kotlin/com/sandra/slack_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sandra.slack_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/slacks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.slacks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
